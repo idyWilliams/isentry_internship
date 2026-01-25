ea4e01aad33a42bb8608a5a10a0a23129ad4f6a5
idyWilliams <widorenyin0@gmail.com>
2026-01-26 00:38:35 +0100

    update themeing

--- app/layout.tsx
+++ app/layout.tsx
@@ -52,8 +52,8 @@ export default function RootLayout({
       <body className={`${helix.variable} font-sans antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
-          enableSystem
+          defaultTheme="light"
+          enableSystem={false}
           disableTransitionOnChange
         >
           <div className="flex min-h-screen flex-col">
